a91427acec0f6176b44d5dc86266a34061437468
Luca Sorrentino <sorrentinoluca.1999@gmail.com>
2023-08-11 19:39:15 +0200

    feat: helper functions to encode responses

--- handlers/healtz.go
+++ handlers/healtz.go
@@ -3,7 +3,7 @@ package handlers
 import "net/http"
 
 func HandleHealtz(w http.ResponseWriter, r *http.Request) {
-	WriteJsonResponse(w, http.StatusOK, map[string]string{
+	RespondWithJSON(w, http.StatusOK, map[string]string{
 		"status": "ok",
 	})
 }
--- handlers/json.go
+++ handlers/json.go
@@ -2,14 +2,29 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
-func WriteJsonResponse(w http.ResponseWriter, status int, data interface{}) {
+func RespondWithJSON(w http.ResponseWriter, status int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error encoding JSON response: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+	w.WriteHeader(status)
+}
+
+func RespondWithError(w http.ResponseWriter, status int, message string) {
+	if status >= 500 {
+		log.Printf("Error: %v\n", message)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	RespondWithJSON(w, status, errResponse{Error: message})
 }
